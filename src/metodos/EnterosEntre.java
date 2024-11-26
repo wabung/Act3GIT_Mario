9b98aea895690fd3f5f2b13e9ec19952e723f43b
Mario <Mario@DESKTOP-59SKDE3>
2024-11-26 20:40:16 +0100

    Modifico el programa EnterosEntre

--- src/metodos/EnterosEntre.java
+++ src/metodos/EnterosEntre.java
@@ -19,12 +19,21 @@ public class EnterosEntre {
 		            return;
 		        }
 		        
+		        System.out.println("Introduce un tercer número y lo imprimiré si esta comprendido entre los dos anteriores.");
+		        int c = scanner.nextInt();		        
+		        
 		        int menor = a < b ? a : b;
 		        int mayor = a > b ? a : b;
 		        
 		        System.out.println("Números enteros entre " + menor + " y " + mayor + " incrementando de 7 en 7:");
 		        for (int i = menor; i <= mayor; i += 7) {
 		            System.out.println(i);
+		        }
+		        
+		        for (int i = a; i <= b; i += 1) {
+		        	if (i == c) {
+		        		System.out.println(c);
+		        	}
 		        }
 	}
 
